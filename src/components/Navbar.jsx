08a470c959e5c57582c98c2ab58dfcb99c3fd7c1
Syed Mahib <syedmahib8@gmail.com>
2025-05-02 19:07:54 +0600

    made some changes in the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,14 +6,14 @@ const Navbar = () => {
     return (
         <div className='flex justify-between items-center'>
             <div></div>
-            <div className='text-accent flex justify-center items-center gap-5 text-lg ml-[150px]'>
-                <NavLink to="/home" className={({isActive})=> isActive ? "text-secondary" : "text-primary"}>Home</NavLink>
-                <NavLink to="/about" className={({isActive})=> isActive ? "text-secondary" : "text-primary"}>About</NavLink>
-                <NavLink to="/carrear" className={({isActive})=> isActive ? "text-secondary" : "text-primary"}>Carrear</NavLink>
+            <div className='text-accent flex justify-center items-center gap-5 text-lg ml-[190px]'>
+                <NavLink to="/" className={({isActive})=> isActive ? "text-secondary font-bold" : "text-primary"}>Home</NavLink>
+                <NavLink to="/about" className={({isActive})=> isActive ? "text-secondary font-bold" : "text-primary"}>About</NavLink>
+                <NavLink to="/carrear" className={({isActive})=> isActive ? "text-secondary font-bold" : "text-primary"}>Carrear</NavLink>
             </div>
             <div className='flex gap-3'>
                 <img src={avatar} alt=""/>
-                <button className='btn bg-primary text-base-100 text-xl font-semibold'>Login</button>
+                <button className='btn btn-primary text-base-100 text-xl font-semibold px-10'>Login</button>
             </div>
         </div>
     );
